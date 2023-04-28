3f38e3d61b9eb9adc76c9e53dc834ce3b4ac48fd
hotmangasi94@gmail.com <hotmangasi94@gmail.com>
2023-04-28 16:03:04 +0700

    updated

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ from keras.models import load_model
 import sklearn
 import pandas as pd
 import tensorflow as tf
+import pickle
 
 app = Flask(__name__)
 # Load model from file
 model = load_model('ann.h5')
 # load data
 data = pd.read_csv('data_without_norm.csv')
+
+# Load svr model from file
+with open('svr.pkl', 'rb') as f:
+    svr_model = pickle.load(f)
 
 # define function to get previous year enrollments
 def get_previous_enrollments(school_name):
@@ -26,6 +31,14 @@ def home():
     # get unique school names
     school = data['Nama Sekolah'].unique().tolist()
     return render_template('index.html', school=school)
+# Define the svr route
+@app.route('/svr')
+def svr():
+    return render_template('svr-view.html')
+# Define the ann route
+@app.route('/ann')
+def ann():
+    return render_template('ann-view.html')
 
 # Define the prediction route
 @app.route('/predict', methods=['GET','POST'])
@@ -36,13 +49,19 @@ def predict():
     # get previous enrollments
     enrollments = get_previous_enrollments(school_name)
     print('Jumlah pendaftar tahun sebelumnya',enrollments)
-    # make prediction for 2021
-    prediction = model.predict([enrollments])[0][0]
-    print('Hasil prediksi',prediction)
+    # make prediction using ANN model
+    ann_prediction = model.predict([enrollments])[0][0]
+    print('Hasil prediksi ANN',ann_prediction)
+    # make prediction using SVR model
+    svr_prediction = svr_model.predict([enrollments])[0]
+    print('Hasil prediksi SVR',svr_prediction)
     # round prediction to nearest integer
-    prediction = int(prediction)
+    ann_prediction = int(ann_prediction)
+    svr_prediction = int(svr_prediction)
     # return prediction to user
-    return render_template('index.html', schools=data['Nama Sekolah'].unique().tolist(), prediction=prediction, school=school_name)
+    return render_template('svr-view.html', schools=data['Nama Sekolah'].unique().tolist(), 
+                           ann_prediction=ann_prediction, svr_prediction=svr_prediction, 
+                           school=school_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
